309387da6031cdf44f73cdcac693b9ef4bef327b
Brenda Wallace <shiny@cpan.org>
2009-07-04 19:17:06 +1200

    Drupal Theme

---
+++ page.tpl.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+   <html xmlns="http://www.w3.org/1999/xhtml">
+   <head profile="http://gmpg.org/xfn/11">
+   <meta http-equiv="Content-Type" content="text/html; charset=windows-1250" />
+      <title> 
+         forte green
+      </title>
+   <meta name="copyright" content="" />
+   <meta name="robots" content="index,follow" />
+   <meta name="author" content=")" />
+   <meta name="keywords" content="" />
+   <meta name="description" content="" />
+   <meta name="generator" content="" />
+<link rel="stylesheet" href="../forte_green/design.css" type="text/css" media="screen" />
+
+<!--[if gte IE 6]> 
+<link rel="stylesheet" type="text/css" href="../forte_green/ie.css" media="screen, projection" /> 
+<![endif]-->
+
+<!--[if IE 7.0]>
+<link rel="stylesheet" type="text/css" href="../forte_green/ie7.css" media="screen, projection" />
+<![endif]-->
+
+</head>
+  
+<body>
+<div id="site">
+
+<div id="logo">
+   <div id="logoimage">
+<h1><a href="#"><span class="color1">Logo</span><span class="color2">Type</span></a></h1>
+   </div>
+</div>
+
+<ul id="menu">
+<li id="home"><a href="#">Home</a></li>
+<li id="contact"><a href="#">Contact</a></li>
+<li id="about"><a href="#">About</a></li>
+<li id="help"><a href="#">Help</a></li>
+</ul>
+
+<hr />
+
+
+<div id="list">
+<ol>
+<li id="num1">Lorem <strong>ipsum dolor</strong> sit amed</li>
+<li id="num2">gravida tuttus, ut dignissim <strong>leo</strong></li>
+<li id="num3">nam <strong>diam</strong> purus, aliquam</li>
+</ol>
+</div>
+
+
+<div id="slogan">
+<h2>
+Business is not alike<br /> ...but your profit is...
+</h2>
+</div>
+
+<div id="backg">
+
+<div id="button">
+<h2><a href="#">learn more</a></h2>
+</div>
+
+<hr />
+
+<ul id="menu2">
+<li id="info"><a href="#">info</a></li>
+<li id="product"><a href="#">product</a></li>
+<li id="service"><a href="#">service</a></li>
+<li id="blog"><a href="#">blog</a></li>
+</ul>
+
+<hr />
+
+
+<div id="line1">
+</div>
+
+<div id="news">
+
+<h3>What’s news</h3>
+
+<p class="date1">2nd of August 2008</p>
+  <p class="news1">
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+    Nam diam purus, aliquam non, ultrices in, viverra nec, ipsum. 
+    Mauris eu mi eget dolor tristique interdum. Pellentesque 
+    elementum, tellus sit amet sodales dignissim, enim urna gravida 
+    tellus, ut dignissim leo risus ut ligula. Maecenas tempor elit nec 
+    enim. Cras dictum metus non turpis. Aenean gravida. Nunc et 
+    odio non risus imperdiet nonummy.
+  </p>
+
+<p class="date2">1nd of August 2008</p>
+   <p class="news2">
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+    Nam diam purus, aliquam non, ultrices in, viverra nec, ipsum. 
+    Mauris eu mi eget dolor tristique interdum. Pellentesque 
+    elementum, tellus sit amet sodales dignissim,
+  </p>
+
+<p class="date3">1nd of July 2008</p>
+  <p class="news3">
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+    Nam diam purus, aliquam non, ultrices.
+  </p>
+
+</div>
+
+
+<div id="old">
+<a href="#">old news>></a>
+</div>
+
+
+<hr />
+<div id="contect">
+<p class="contect1">
+Lorem ipsum dolor sit amet, consectetuer. gravida tellus, ut dignissim leo risus ut ligula. urna gravida tellus, ut dignissim leo risus ut ligula.
+ellus sit amet sodales dignissim, enim urna gravida. ipsum. Mauris eu mi eget dolor tristique interdum. ultrices in, viverra nec, ipsum. Mauris eu mi eget.
+Nam diam purus, aliquam non, ultrices in, viverra.
+</p>
+</div>
+
+
+<div id="imagesmall">
+<img src="image_small.png" alt="image small" />
+</div>
+
+<div id="imagebig">
+<img src="image_big.png" alt="image big" />
+</div>
+
+          
+            <table border="1" cellpadding="0" cellspacing="0">
+              <thead>
+              	<tr>
+              	<th>Legend</th>
+                  <th>Legend</th>
+                  <th>Legend</th>
+              	</tr>
+
+              </thead>
+              
+              <tbody>
+              	<tr>
+
+              		<td>text</td>
+              		<td>text</td>
+              		<td>text</td>
+                </tr>
+              	<tr>              
+              		<td>text</td>
+              		<td>text</td>
+                        <td>text</td>
+                </tr>
+              	<tr>
+              		<td>text</td>
+              		<td>text</td>
+              		<td>text</td>
+                </tr>
+
+              	<tr>
+              		<td>text</td>
+              		<td>text</td>
+              		<td>text</td>
+                </tr>
+              </tbody>
+            </table>
+
+<hr />
+
+</div>
+
+<div id="footer1">
+<p class="footer">
+designed: <a href="http://www.fakam.sk">fakam.sk</a> <br />sponsored: <a href="http://www.hrajsa.sk" title="online hry, plne hry, zadarmo">Free online games</a>, Thanks to <a href="http://www.help4diabetics.com" target="_blank">Diabetic Supplies</a>
+</p>
+</div>
+
+</div>
+
+<div id="footer_line">
+</div>
+
+</body>
+</html>
